07c1ea648a27361d0195c13a3710b942a146c050
Franco Bosetti <francobosetti@live.com.ar>
2023-10-12 16:57:01 -0300

    chore: import data from csv

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -3,8 +3,18 @@ import csv
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-#TODO import data from csv
-data=[]
+data = {}
+
+with open('../data/europe.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+            continue
+        else:
+            data[row[0]] = [[i for i in row[1:8]]]
+            line_count += 1
 
 scaling=StandardScaler()
 
