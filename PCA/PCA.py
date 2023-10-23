0169646d46ef76e7c346aed4c8302b0247a6f1ce
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-10-23 15:59:19 -0300

    fix: PCA barplot

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -106,43 +106,22 @@ def create_original_vs_standardized_boxplots(
     plt.savefig("standardized_data_boxplot.png")
 
 
-def create_first_component_biplot(
-    pca_result: NDArray, pca: PCA, countries: List[str], variables: List[str]
+def create_first_component_barplot(
+    pca_result: NDArray,  countries: List[str]
 ):
     pc1 = pca_result[:, 0]
 
-    # Pesos
-    pc1_loadings = pca.components_[0, :]
 
-    plt.figure(figsize=(10, 6))
-    plt.scatter(pc1, np.zeros_like(pc1), marker="o", label="PC1 Scores", color="blue")
+    plt.figure(figsize=(18, 14))
+    plt.bar(countries, pc1, label="PC1 Scores", color="blue")
 
-    # Plot the variable loadings as vectors
-    y_offset = 0.02
-    for i, (loading, variable) in enumerate(zip(pc1_loadings, variables)):
-        plt.arrow(0, y_offset * (i + 1), loading, 0, color="red", alpha=0.7)
-
-        text_x = loading + 0.02 if loading > 0 else loading - 0.09 * len(variable)
-
-        plt.text(text_x, y_offset * (i + 1), variable, fontsize=12, color="red")
-
-    for i, score in enumerate(pc1):
-        plt.annotate(
-            countries[i],
-            (score, 0),
-            xytext=(0, -70),
-            textcoords="offset points",
-            fontsize=8,
-            ha="right",
-            rotation=90,
-        )
-
-    plt.xlabel("PC1 Scores")
-    plt.ylim(-0.05, 0.2)
+    plt.xlabel("Countries")
+    plt.ylabel("PC1 Scores")
+    plt.xticks(rotation=90)
     plt.legend()
     plt.grid()
-    plt.title("Biplot: PC1 Scores and Variable Loadings")
-    plt.savefig("first_component_biplot.png")
+    plt.title("Barplot: PC1 Scores and Variable Loadings")
+    plt.savefig("first_component_plot.png")
 
 
 def create_first_second_component_biplot(
@@ -213,5 +192,5 @@ pca = PCA(n_components=2)
 pca.fit(scaled_data)
 x = pca.transform(scaled_data)
 
-create_first_component_biplot(x, pca, country_names, variables)
+create_first_component_barplot(x, country_names)
 create_first_second_component_biplot(x, pca, country_names, variables)
